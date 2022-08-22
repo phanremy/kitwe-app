76b6861b75ddecb73fb65a52c073a02deefd336d
remy phan <phan.remy@gmail.com>
2022-08-22 10:28:42 +0200

    reveal controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,8 +5,10 @@ import { Application } from "@hotwired/stimulus"
 import ModalDisplayController from "controllers/modal_display_controller"
 import ModalOpenController from "controllers/modal_open_controller"
 import FlashCloseController from "controllers/flash_close_controller"
+import RevealController from "controllers/reveal_controller"
 
 window.Stimulus = Application.start()
 Stimulus.register("modal-open", ModalOpenController)
 Stimulus.register("modal-display", ModalDisplayController)
 Stimulus.register("flash-close", FlashCloseController)
+Stimulus.register("reveal", RevealController)
