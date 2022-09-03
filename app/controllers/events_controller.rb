0d47acf5e7460131177630a98dc6653817015507
remy phan <phan.remy@gmail.com>
2022-09-04 00:19:48 +0200

    events controller was not defined

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,68 +1,68 @@
 # frozen_string_literal: true
 
 # # top level documentation for EventsController
-# class EventsController < ApplicationController
-#   load_and_authorize_resource
+class EventsController < ApplicationController
+  #   load_and_authorize_resource
 
-#   before_action :authenticate_user!
+  #   before_action :authenticate_user!
 
-#   def index
-#     @events = Event.all
-#   end
+  #   def index
+  #     @events = Event.all
+  #   end
 
-#   def show
-#     @event = Event.find(params[:id])
-#   end
+  #   def show
+  #     @event = Event.find(params[:id])
+  #   end
 
-#   def new
-#     @event = Event.new
-#   end
+  #   def new
+  #     @event = Event.new
+  #   end
 
-#   def create
-#     @event = Event.new(event_params)
-#     if @event.save
-#       flash[:success] = 'Event successfully created'
-#       redirect_to @event
-#     else
-#       flash.now[:error] = @event.errors.full_messages
-#       render_flash
-#     end
-#   end
+  #   def create
+  #     @event = Event.new(event_params)
+  #     if @event.save
+  #       flash[:success] = 'Event successfully created'
+  #       redirect_to @event
+  #     else
+  #       flash.now[:error] = @event.errors.full_messages
+  #       render_flash
+  #     end
+  #   end
 
-#   def edit; end
+  #   def edit; end
 
-#   def update
-#     if @event.update(event_params)
-#       # ProfileEvent.find_or_create
-#       flash.now[:success] = 'Event was successfully updated'
-#       redirect_to @event
-#     else
-#       flash.now[:error] = @event.errors.full_messages
-#       render_flash
-#     end
-#   end
+  #   def update
+  #     if @event.update(event_params)
+  #       # ProfileEvent.find_or_create
+  #       flash.now[:success] = 'Event was successfully updated'
+  #       redirect_to @event
+  #     else
+  #       flash.now[:error] = @event.errors.full_messages
+  #       render_flash
+  #     end
+  #   end
 
-#   def destroy
-#     if @event.destroy
-#       flash[:success] = 'Event was successfully deleted.'
-#       redirect_to events_path
-#     else
-#       flash.now[:error] = 'Something went wrong'
-#       redirect_to @event
-#     end
-#   end
+  #   def destroy
+  #     if @event.destroy
+  #       flash[:success] = 'Event was successfully deleted.'
+  #       redirect_to events_path
+  #     else
+  #       flash.now[:error] = 'Something went wrong'
+  #       redirect_to @event
+  #     end
+  #   end
 
-#   private
+  #   private
 
-#   def current_ability
-#     @current_ability ||= ::Ability.new(current_user)
-#   end
+  #   def current_ability
+  #     @current_ability ||= ::Ability.new(current_user)
+  #   end
 
-#   def set_event
-#     @event = Event.find(params[:id])
-#   end
+  #   def set_event
+  #     @event = Event.find(params[:id])
+  #   end
 
-#   def event_params
-#     params.require(:event).permit(Event::FORM_ATTRIBUTES)
-#   end
-# end
+  #   def event_params
+  #     params.require(:event).permit(Event::FORM_ATTRIBUTES)
+  #   end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 # # top level documentation for Event
-# class Event < ApplicationRecord
-#   has_paper_trail
+class Event < ApplicationRecord
+  # has_paper_trail
 
-#   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  # belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
-#   has_many :profile_events,           dependent: :delete_all
-#   has_many :profiles,                 inverse_of: :events, through: :profile_events
+  # has_many :profile_events,           dependent: :delete_all
+  # has_many :profiles,                 inverse_of: :events, through: :profile_events
 
-#   CATEGORIES = %w[birthday wedding].freeze
-#   PRIVACIES = %w[public only_involved only_shared only_friends private].freeze
-#   FORM_ATTRIBUTES = %w[creator_id category privacy date].freeze
+  # CATEGORIES = %w[birthday wedding].freeze
+  # PRIVACIES = %w[public only_involved only_shared only_friends private].freeze
+  # FORM_ATTRIBUTES = %w[creator_id category privacy date].freeze
 
-#   validates :category,
-#             inclusion: { in: Event::CATEGORIES }
+  # validates :category,
+  #           inclusion: { in: Event::CATEGORIES }
 
-#   validates :privacy,
-#             inclusion: { in: Event::PRIVACIES }
-# end
+  # validates :privacy,
+  #           inclusion: { in: Event::PRIVACIES }
+end
