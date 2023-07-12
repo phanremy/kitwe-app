59d1ec313396bf3188849a148bb52fb44615b2d8
remy phan <phan.remy@gmail.com>
2023-07-12 21:45:59 +0200

    improve front of profile outline

--- app/helpers/couples_helper.rb
+++ app/helpers/couples_helper.rb
@@ -2,6 +2,12 @@
 
 # top level description of CouplesHelper
 module CouplesHelper
+  def child_of(parents)
+    content_tag(:div, class: 'flex') do
+      partners_links(parents)
+    end
+  end
+
   def partners_links(couple)
     content = couple.partners.map do |partner|
       next unless partner
@@ -10,5 +16,15 @@ module CouplesHelper
     end.compact
 
     safe_join(content, raw('<div class="mx-1"> & </div>'))
+  end
+
+  def other_partner_link(couple, profile)
+    other_partner = couple.other_partner(profile)
+
+    if other_partner.blank?
+      'Single'
+    else
+      link_to(other_partner.designation, profile_path(other_partner.id, profile_id: nil))
+    end
   end
 end
