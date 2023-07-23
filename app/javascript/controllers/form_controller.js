9c6f524d47ab20d0e5031a93c27ada2778820393
remy phan <phan.remy@gmail.com>
2023-07-23 17:15:51 +0200

    add quality of life features

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,12 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect() {
-    console.log('form_submit_controller - start')
-  }
-
   proceed(event) {
-    console.log(event)
     this.element.requestSubmit()
   }
 }
--- app/javascript/controllers/tree_interaction_controller.js
+++ app/javascript/controllers/tree_interaction_controller.js
@@ -4,10 +4,6 @@ export default class extends Controller {
   static outlets = ['form']
   static targets = ['node']
 
-  connect() {
-    console.log('tree_interaction_controller - start')
-  }
-
   onNodeClick(event) {
     if (!this.nodeTargets.includes(event.target)) {
       return
@@ -15,11 +11,8 @@ export default class extends Controller {
 
     this.nodeTargets.forEach((node) => { node.classList.remove('solid__node__isRoot') })
     event.target.classList.toggle('solid__node__isRoot')
-    const formOutlineId = document.getElementById('tree-interaction-id')
 
-    if (formOutlineId.value !== event.target.dataset.nodeId) {
-      formOutlineId.value = event.target.dataset.nodeId
-      this.formOutlet.proceed(`form submit ${event.target.dataset.nodeId}`)
-    }
+    document.getElementById('tree-interaction-id').value = event.target.dataset.nodeId
+    this.formOutlet.proceed(`form submit ${event.target.dataset.nodeId}`)
   }
 }
