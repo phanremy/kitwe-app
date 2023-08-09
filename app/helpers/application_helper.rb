e6fb529ae7f9dd2a75280ce99fb280f4273fd33d
remy phan <phan.remy@gmail.com>
2023-08-09 09:51:35 +0200

    add language switcher

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
   def non_breaking(string)
     string.sub(' ', [NARROW_NON_BREAKING_SPACE].pack('U*'))
   end
+
+  def switch_language
+    switch_locale == :fr ? 'Français' : 'English'
+  end
+
+  def switch_locale
+    params[:locale].to_sym == :en ? :fr : :en
+  end
+
+  def switch_locale_path
+    url_for(locale: switch_locale, token: params[:token])
+  end
 end
