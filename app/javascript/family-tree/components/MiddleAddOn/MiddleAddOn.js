28726ba737ef46180abf581bfb77f7a231966686
remy phan <phan.remy@gmail.com>
2023-08-17 10:17:10 +0200

    front of middle addon done

--- app/javascript/family-tree/components/MiddleAddOn/MiddleAddOn.js
+++ app/javascript/family-tree/components/MiddleAddOn/MiddleAddOn.js
@@ -9,22 +9,16 @@ function calcSide(size, factor, thickness) {
 function MiddleAddOn({ middleAddOn, width, height, thickness = MIN_THICKNESS }) {
   const [x1, y1, x2, y2] = middleAddOn;
 
-  console.group('MiddleAddOn');
-    console.log('middleAddOn', middleAddOn);
-    console.log('width', width);
-    console.log('height', height);
-  console.groupEnd();
-
-  // y stay the same
-  // x need to be reworked
+  // horizontal and vertical offsets for 1 bar in the middle (separated)
+  // 2 bars surrounding the middle (divorced)
   return (
     html`<i
           style=${{
             position: 'absolute',
-            width: calcSide(x2 - x1, width, thickness) + 'px',
-            height: calcSide(y2 - y1, height, thickness) + 'px',
-            background: "red",
-            transform: `translate(${x1 * width - (thickness / 2)}px, ${y1 * height - (thickness / 2)}px) rotate(130deg)`,
+            width: '20px',
+            height: '1px',
+            background: 'black',
+            transform: `translate(${(x1 + x2)/2 * width - (thickness / 2) - 8}px, ${y1 * height - (thickness / 2) + 2}px) rotate(130deg)`,
             pointerEvents: 'none',
           }}
         />`
