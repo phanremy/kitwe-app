ea7d2677598a824cb9e59499e766a05963bff1aa
remy phan <phan.remy@gmail.com>
2023-08-21 20:03:03 +0200

    fix locale scope and improve profile cards front

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'pages#homepage'
-  resource :modal_shared_links, only: :create
-  resource :shared_links, only: :create
-  resource :downloads, only: :create
-
-  draw :profile
-
   scope '(:locale)', locale: /fr|en/ do
+    root 'pages#homepage'
+    resource :modal_shared_links, only: :create
+    resource :shared_links, only: :create
+    resource :downloads, only: :create
+
+    draw :profile
+
     resources :profiles, only: %i[index show] do
       resources :couples, controllers: 'couples', except: %i[show destroy]
 
@@ -26,10 +26,10 @@ Rails.application.routes.draw do
 
     draw :outline
     resources :users, only: %i[index edit update destroy]
+
+    resources :couples, only: %i[destroy]
+
+    get '/open-modal', to: 'pages#open_modal', as: 'open_modal'
+    # resources :posts
   end
-
-  resources :couples, only: %i[destroy]
-
-  # resources :posts
-  get '/open-modal', to: 'pages#open_modal', as: 'open_modal'
 end
