dc657321a8375c61735146ab0fd460373b6cf53f
remy phan <phan.remy@gmail.com>
2023-08-30 09:08:07 +0200

    frontpage without margin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
 
   NARROW_NON_BREAKING_SPACE = 8239
 
+  def frontpage?
+    params[:controller] == 'pages' && params[:action] == 'frontpage'
+  end
+
   def non_breaking(string)
     string.sub(' ', [NARROW_NON_BREAKING_SPACE].pack('U*'))
   end
