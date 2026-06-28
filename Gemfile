8dbafce3c0fe9cc873693dad2d8f3c1b92e90685
remy phan <phan.remy@gmail.com>
2026-06-29 01:50:13 +0200

    install sentry

--- Gemfile
+++ Gemfile
@@ -52,6 +52,11 @@ gem 'turbo-rails'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+# sentry
+gem "stackprof"
+gem "sentry-ruby"
+gem "sentry-rails"
+
 group :development, :test do
   # Use bullet for detecting N+1 query
   gem 'bullet'
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,26 @@
+Sentry.init do |config|
+  config.dsn = 'https://6daae591e0f2ef745e31155360fb4b46@o4511645235740672.ingest.de.sentry.io/4511645260251216'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+
+  # Enable sending logs to Sentry
+  config.enable_logs = true
+  # Patch Ruby logger to forward logs
+  config.enabled_patches = [:logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
